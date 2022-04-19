dc228e7b57a3adea0b92fbf8e31b784c5c970c53
just2code <just2code19@gmail.com>
2022-04-19 12:11:42 +0530

    first commit

---
+++ src/main/java/LearningCICD/ContinuosIntegration/App.java
@@ -0,0 +1,13 @@
+package LearningCICD.ContinuosIntegration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/LearningCICD/ContinuosIntegration/firstTest.java
@@ -0,0 +1,13 @@
+package LearningCICD.ContinuosIntegration;
+
+import org.testng.annotations.Test;
+
+public class firstTest {
+	
+	@Test
+	public void testThis()
+	{
+		System.out.println("In");
+	}
+
+}
